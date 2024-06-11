893bfd9f7bab87f1ab56ec56bbec04724e444ebb
Inheon Choi <46120405+ih9511@users.noreply.github.com>
2024-06-11 19:52:19 +0900

    [Silver IV] Title: 수열, Time: 76 ms, Memory: 41332 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.py
+++ 백준/Silver/2491. 수열/수열.py
@@ -3,8 +3,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-li = list(map(int, input().split(' ')))
+li = list(map(int, input().split(" ")))
 count = []
+
 
 def asc(asc_list):
     # li 순회하면서 커지는 수열 길이 세는 함수
@@ -12,15 +13,14 @@ def asc(asc_list):
     cnt = 1
     for i in asc_list[1:]:
         if i >= a:
-            # print('a')
             cnt += 1
             a = i
         else:
-            # print('b')
             count.append(cnt)
             a = i
             cnt = 1
         count.append(cnt)
+
 
 def des(dec_list):
     # li 순회하면서 작아지는 수열 길이 세는 함수
@@ -28,20 +28,18 @@ def des(dec_list):
     cnt = 1
     for i in dec_list[1:]:
         if i <= d:
-            # print('c')
             cnt += 1
             d = i
         else:
-            # print('d')
             count.append(cnt)
             d = i
             cnt = 1
         count.append(cnt)
+
+
 asc(li)
-# count.append(999999)
-# print('asc -> des')
 des(li)
-# print(count)
+
 if n == 1:
     print(1)
 else:
