f175d429354ee1ceddb8ab57f87a3783cb57898c
Inheon Choi <46120405+ih9511@users.noreply.github.com>
2024-07-16 18:46:53 +0900

    [Bronze V] Title: 사분면 고르기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+
+x = int(input().strip())
+y = int(input().strip())
+
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
