60add89ff870e9b348dac14ffda155196b41172d
Inheon Choi <46120405+ih9511@users.noreply.github.com>
2024-09-26 19:59:54 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 113944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,39 +1,36 @@
-# https://www.acmicpc.net/problem/2178
-
-import sys
-
 from collections import deque
 
-input = sys.stdin.readline
-
-N, M = map(int, input().strip().split())
-
+N, M = map(int, input().split())
 board = []
+
 for _ in range(N):
-    board.append(list(input()))
-    
-visited = [[False] * M for _ in range(N)]
-ans = [[0] * M for _ in range(N)]
+    board.append(input())
+
+visited = [[-1] * M for _ in range(N)]
 
 queue = deque()
 queue.append((0, 0))
-visited[0][0] = True
-ans[0][0] = 1
+visited[0][0] = 1
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 while queue:
     x, y = queue.popleft()
     
-    for nx, ny in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        
+        if nx < 0 or ny < 0 or nx >= N or ny >= M:
             continue
-        # 0이면 이동 불가
-        if board[nx][ny] =='0':
+        
+        if board[nx][ny] == '0':
             continue
-        # 만약 다음에 이동하고자 하는 칸이 방문되지 않은 칸이라면,
-        if not visited[nx][ny]:
-            visited[nx][ny] = True
-            # 다음에 방문하는 칸은 이전에 방문한 칸보다 딱 한 칸만 더 이동하면 됨
-            ans[nx][ny] = ans[x][y] + 1
-            queue.append([nx, ny])
-            
-print(ans[N - 1][M - 1])
+        
+        if visited[nx][ny] != -1:
+            continue
+        
+        visited[nx][ny] = visited[x][y] + 1
+        queue.append((nx, ny))
+        
+print(visited[N - 1][M - 1])
